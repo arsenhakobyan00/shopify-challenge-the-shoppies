cc07e6619834c1df9872ee83fc90b4ffffe084df
Arsen Hakobyan <arsen.hak01@gmail.com>
2021-12-03 04:25:18 -0500

    final

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'cache-v1';
-const urlsToCache = ['../index.html', '../offline.html'];
+const urlsToCache = ['index.html', 'offline.html'];
 
 const self = this;
 
@@ -21,7 +21,7 @@ self.addEventListener('fetch', (event) => {
         caches.match(event.request)
             .then(() => {
                 return fetch(event.request)
-                    .catch(() => caches.match('../offline.html'))
+                    .catch(() => caches.match('offline.html'))
             })
     )
 });
